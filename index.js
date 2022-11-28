0a75d734da8eca0cd35d034ac6ef357d52895254
tracykagai-2022 <tracy.kagai@student.moringaschool.com>
2022-11-28 05:17:25 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = "BOB"
+}
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "a";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "b";
+}
